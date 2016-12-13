a533a0fcafee61c2d3ad389b891bf0b879fa9e1d
Nick DeJesus <ndejesus@sapient.com>
2016-12-13 10:01:01 -0500

    renaming

---
+++ src/components/CharacterSelect/CharacterSelect.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View, Picker } from 'react-native';
+
+export default class Dropdown React.Component {
+  render() {
+
+    return (
+      <Picker>
+
+      </Picker>
+    )
+  }
+}
