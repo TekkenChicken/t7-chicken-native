a393cbf10d4e7f274e7bef0fe3221e62da9ec477
Nick DeJesus <ndejesus@sapient.com>
2017-04-10 06:15:49 -0400

    added MoveList component

--- src/containers/CharacterProfile/index.js
+++ src/containers/CharacterProfile/index.js
@@ -16,24 +16,30 @@ import {
 import ProfileBanner from '../../components/CharacterProfile/ProfileBanner';
 import ProfilePicture from '../../components/CharacterProfile/ProfilePicture';
 import ProfileName from '../../components/CharacterProfile/ProfileName';
+import MoveList from './MoveList';
 // Styles
 import Styles from './styles';
 
 // dispatch actions
+import { fetchCharacters } from '../../redux/actions/select';
+import { fetchInitialAppData } from '../../redux/actions/blob';
 
 
 class CharacterProfileScreen extends Component {
 
   componentDidMount() {
     // Fetch Data on character Or pass it in as props from navigation?
+    this.props.dispatch(fetchInitialAppData());
+    this.props.dispatch(fetchCharacters());
   }
 
   render() {
     return (
       <ScrollView style={Styles.mainContainer}>
         <ProfileBanner />
-      <ProfilePicture image='./../../img/Tile-Kazuya.png' />
+        <ProfilePicture image='./../../img/Tile-Kazuya.png' />
         <ProfileName name="Kazuya Mishima" />
+        <MoveList />
       </ScrollView>
     );
   }
@@ -42,8 +48,9 @@ class CharacterProfileScreen extends Component {
 
 /** MAPPING STATE **/
 const mapStateToProps = function(state) {
+  let { characters } = state.select;
   return {
-    character : "Feng"
+    characters
   }
 };
 
