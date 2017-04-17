229710d16ce7b1f5faac99471ac0ba9f12fabc3c
Scott Jones <scottdj92@gmail.com>
2017-04-17 12:00:02 -0400

    Add requires for all input images and hook it up to component

--- src/components/Inputs/Inputs.js
+++ src/components/Inputs/Inputs.js
@@ -5,6 +5,8 @@ import { View,
   Image
 } from 'react-native';
 
+import inputs from '../../img/inputs/index';
+
 export default class Inputs extends Component {
   constructor() {
     super();
@@ -12,58 +14,57 @@ export default class Inputs extends Component {
   }
 
   renderInputs = (inputs) => {
-    // TODO: figure out the best way to implement keys here so RN will stop bitching
     return inputs.map((input, index) => {
       switch (input) {
         // TODO: add image sources here
         case '1':
           return (
-            <Image></Image>
+            <Image key={index}></Image>
           );
           break;
         case '2':
           return (
-            <Image></Image>
+            <Image key={index}></Image>
           );
           break;
         case '3':
           return (
-            <Image></Image>
+            <Image key={index}></Image>
           );
           break;
         case '4':
           return (
-            <Image></Image>
+            <Image key={index}></Image>
           );
           break;
         case '1+2':
           return (
-            <Image></Image>
+            <Image key={index}></Image>
           );
           break;
         case '1+3':
           return (
-            <Image></Image>
+            <Image key={index}></Image>
           );
           break;
         case '1+4':
           return (
-            <Image></Image>
+            <Image key={index}></Image>
           );
           break;
         case '1+2+3':
           return (
-            <Image></Image>
+            <Image key={index}></Image>
           );
           break;
         case '1+2+4':
           return (
-            <Image></Image>
+            <Image key={index}></Image>
           );
           break;
         case '1+2+3+4':
           return (
-            <Image></Image>
+            <Image key={index}></Image>
           );
           break;
         case '2+4':
@@ -73,52 +74,52 @@ export default class Inputs extends Component {
           break;
         case '3+4':
           return (
-            <Image></Image>
+            <Image key={index}></Image>
           );
           break;
         case 'u':
           return (
-            <Image></Image>
+            <Image key={index}></Image>
           );
           break;
         case 'd':
           return (
-            <Image></Image>
+            <Image key={index}></Image>
           );
           break;
         case 'b':
           return (
-            <Image></Image>
+            <Image key={index}></Image>
           );
           break;
         case 'f':
           return (
-            <Image></Image>
+            <Image key={index}></Image>
           );
           break;
         case 'u/f':
           return (
-            <Image></Image>
+            <Image key={index}></Image>
           );
           break;
         case 'd/f':
           return (
-            <Image></Image>
+            <Image key={index}></Image>
           );
           break;
         case 'd/b':
           return (
-            <Image></Image>
+            <Image key={index}></Image>
           );
           break;
         case 'u/b':
           return (
-            <Image></Image>
+            <Image key={index}></Image>
           );
           break;
         case 'n':
           return (
-            <Image></Image>
+            <Image key={index}></Image>
           );
           break;
           // TODO: add cases for WR/WS/CH/FC/Stances
