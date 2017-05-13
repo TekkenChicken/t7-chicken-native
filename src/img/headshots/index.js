886d629786bfccb64dc3f708914b7404ae2ec296
Nick DeJesus <ndejesus@sapient.com>
2017-05-13 18:58:45 -0400

    started adding photos

--- src/img/headshots/index.js
+++ src/img/headshots/index.js
@@ -1,5 +1,16 @@
 const headshots = {
-  kazuya: require('./Tile-Kazuya.png')
+  akuma: require('./akuma.png'),
+  asuka: require('./asuka.png'),
+  deviljin: require('./deviljin.png'),
+  feng: require('./feng.png'),
+  heihachi: require('./heihachi.png'),
+  jin: require('./jin.png'),
+  kazumi: require('./kazumi.png'),
+  kazuya: require('./Tile-Kazuya.png'),
+  kuma: require('./kuma.png'),
+  law: require('./law.png'),
+  lee: require('./lee.png'),
+  lili: require('./lili.png'),
 };
 
 export default headshots;
