00a84de825ecfe29fa55ac62b213ce26be380e9c
Danny Nguyen <dnguyen@sapient.com>
2017-05-25 02:09:16 -0400

    Remove exponent dependencies

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,25 +1,9 @@
-import { createNavigationEnabledStore } from '@exponent/ex-navigation';
 import { createStore, applyMiddleware, combineReducers } from 'redux';
 import thunk from 'redux-thunk';
 import createLogger from 'redux-logger';
 import rootReducer from './reducers/index';
 
 const loggerMiddleware = createLogger();
-
-// const store = createStoreWithNavigation(
-//   rootReducer,
-//   applyMiddleware(thunk, loggerMiddleware),
-// );
-
-// const createMainStore = (navReducer) => {
-//   return createStore(
-//     createRootReducer(navReducer),
-//     undefined,
-//     applyMiddleware(thunk, loggerMiddleware)
-//   );
-// };
-//
-// export default createMainStore;
 
 export default createStore(
   rootReducer,
