de69771baaa436d37edba504ad9456ca150b4fa0
Danny Nguyen <dnguyen@sapient.com>
2017-06-05 20:49:15 -0400

    Parsing Logic to turn notation string int array for render
    In the future this should be moved to the server

--- src/components/Inputs/Inputs.js
+++ src/components/Inputs/Inputs.js
@@ -2,11 +2,13 @@ import React, { Component } from 'react';
 
 import { View,
   StyleSheet,
-  Image,
-  Text
+  Image
 } from 'react-native';
 
+import CustomText from '../CustomText/CustomText';
+
 import inputImages from '../../img/inputs/index';
+import InputParser from './inputParser';
 
 export default class Inputs extends Component {
   constructor() {
@@ -14,30 +16,47 @@ export default class Inputs extends Component {
     this.state = {};
   }
 
-  renderInputs = (inputs) => {
+  renderInputs(inputs) {
     return inputs.map((input, index) => {
-      return (
-        <Image style={this.props.isCard ? Styles.isCard : Styles.inputChild} source={inputImages[input]} key={index}/>
-      );
+      if (inputImages[input]) {
+        return (
+          <Image style={this.props.isCard ? Styles.isCard : Styles.inputChild}
+            source={inputImages[input]}
+            key={index}/>
+        );
+      } else {
+        return (
+          <CustomText
+            textStyle={Styles.text}
+            key={index}>
+            {input}
+          </CustomText>
+        )
+      }
     });
   }
 
   render() {
+    const inputArray = (this.props.inputs) ? InputParser.parseInputFromString(this.props.inputs) : [];
     return (
       <View style={Styles.inputContainer}>
-        {this.renderInputs(this.props.inputs)}
+        {this.renderInputs(inputArray)}
       </View>
-    )
+    );
   }
 }
 
 const Styles = StyleSheet.create({
   inputContainer: {
+    backgroundColor: "transparent",
     flexDirection: 'row',
     justifyContent: 'flex-end',
-    paddingRight: 10
+    paddingRight: 10,
+    paddingTop: 15,
+    paddingBottom: 15
   },
   inputChild: {
+    marginLeft: 2,
     marginRight: 5,
     height: 24,
     width: 24
@@ -46,5 +65,10 @@ const Styles = StyleSheet.create({
     height: 20,
     width: 20,
     marginTop: 5
+  },
+  text: {
+    marginRight: 1,
+    lineHeight: 20,
+    fontWeight: '500'
   }
 });
