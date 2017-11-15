d65dbf0b4d4d499d848a62bf5a36f1108f717580
dxn7335 <dxn7335@rit.edu>
2017-11-15 03:21:29 -0500

    Preliminary Loading phase when switching to landscape view and loading the spreadsheet

--- src/components/Spreadsheet/index.js
+++ src/components/Spreadsheet/index.js
@@ -26,25 +26,49 @@ class Spreadsheet extends Component {
       };
     }
 
+    componentDidMount() {
+      setTimeout(() => this.triggerLoadState(), 500);
+    }
+
+    triggerLoadState() {
+      this.setState({initialLoad: false});
+    }
+
+
     renderTableHeader() {
       return (
         <FrameDataRow navigation={this.props.navigation} header={true} />
       )
     }
 
-    renderTableRows(moves, navigation) {
-      return moves.map((move, i) =>
-        <FrameDataRow move={move} key={i} rowIndex={i} />
+    // renderTableRows(moves, navigation) {
+    //   return moves.map((move, i) =>
+    //     <FrameDataRow move={move} key={i} rowIndex={i} />
+    //   );
+    // }
+
+    /**
+     *  @method renderTableRow
+     *  @param {object} moveData -- individual move data item provided by FlatList using passed in data source
+     *  @return {component} FrameDataRow
+     *  Callback method passed into FlatList to render a FrameDataRow for each individual move item
+     */
+    renderTableRow(moveData, navigation) {
+      return (
+        <FrameDataRow move={moveData.item} rowIndex={moveData.index} navigation={navigation} />
       );
     }
 
     render() {
       // will be empty on intial load so that rendered Table does not
-      const shownMoves = (this.state.initialLoad) ? [] : moves;
+      const shownMoves = (this.state.initialLoad) ? [] : this.props.moves;
       return (
-        <View>
-          {this.renderTableRows(this.props.moves, this.props.navigation)}
-        </View>
+        <FlatList
+          data={shownMoves}
+          keyExtractor={(move, i) => i}
+          renderItem={(move) => this.renderTableRow(move, this.props.navigation)}
+          ListEmptyComponent={() => (<Text>Loading</Text>)}
+        />
       )
     }
 
